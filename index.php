f8604ab86112129a25b411c6791881260031b455
ranakhas <33785157+ranakhas@users.noreply.github.com>
2019-01-01 19:59:15 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,94 @@
+<!--header-->
+<?php require_once 'includes/header.php'; ?>
+
+<!--navbar-->
+<?php require_once 'includes/navbar.php'; ?>
+
+
+    <div class="container">
+
+        <div class='jumbotron jumbotron-fluid text-center color-set'>
+            <div class="container">
+                <h1 class='display-6'>Project Burger Features</h1>
+                <p class="text-lead">Create the complete login and register form</p>
+            </div>
+        </div>
+
+       <div class="row">
+        <div class="col-md-6">
+
+            <div class='card card-custom'>
+
+
+                <div class="card-header card-header-custom text-center">
+a
+                    <a href="" class="active" id="login-form-link mx-auto">Features [Part - 1]</a>
+                </div>
+                <div class='card-body'>
+
+                    <div class="row">
+                        <div class="col-lg-12">
+
+
+                            <ul class="list-group text-center">
+                                <li class="list-group-item">Login / Register</li>
+                                <li class="list-group-item">Profile mangement System</li>
+                                <li class="list-group-item">Forget/Reset Password</li>
+                                <li class="list-group-item">Remember me Option</li>
+                               
+                            </ul>
+
+
+                        </div>
+                    </div>
+
+
+
+                </div>
+
+
+            </div>
+        </div>
+
+        <div class="col-md-6 ">
+
+            <div class='card card-custom'>
+
+
+                <div class="card-header card-header-custom text-center">
+
+                    <a href="" class="active" id="login-form-link mx-auto">Features [Part - 2]</a>
+                </div>
+                <div class='card-body'>
+
+                    <div class="row">
+                        <div class="col-lg-12">
+
+
+                            <ul class="list-group text-center">
+                                
+                                <li class="list-group-item">Deactivate Account</li>
+                                <li class="list-group-item">Email Verification</li>
+                                <li class="list-group-item">Account Verification</li>
+                                <li class="list-group-item">Unhackable from 12 Year Old Hacker</li>
+                            </ul>
+
+
+                        </div>
+                    </div>
+
+
+
+                </div>
+
+
+            </div>
+        </div>
+</div>
+
+    </div>
+
+<!--footer-->
+<?php require_once 'includes/footer.php'; ?>
+
+   
